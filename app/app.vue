146d119d55d853c7faa16e1db53df69452b5debb
Fabián <faguzman.97@gmail.com>
2025-09-25 10:59:52 -0500

    results mock

--- app/app.vue
+++ app/app.vue
@@ -4,4 +4,9 @@
     <NuxtPage />
   </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import 'primeicons/primeicons.css'
+</script>
+<style>
+@import "tailwindcss-primeui";
+</style>
