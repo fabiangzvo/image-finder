40039f88470a396a5e0cf48f4210e769b02903e0
fabiangzvo <faguzman.97@gmail.com>
2025-09-26 10:32:15 -0500

    add theme button

--- app/app.vue
+++ app/app.vue
@@ -1,11 +1,14 @@
 <template>
-  <div class="relative">
+  <div class="relative dark:bg-black bg-white">
+    <ThemeButton />
     <NuxtRouteAnnouncer />
     <NuxtPage />
   </div>
 </template>
 <script setup lang="ts">
-import 'primeicons/primeicons.css'
+import "primeicons/primeicons.css";
+
+import ThemeButton from "../components/themeButton/index.vue";
 </script>
 <style>
 @import "tailwindcss-primeui";
---
+++ components/themeButton/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <Button class="!absolute left-0 top-0" variant="text" pt:icon:style="font-size:1.3rem; font-weight:bold;"
+    :icon="$colorMode.value !== 'dark' ? 'pi pi-moon' : 'pi pi-sun'" @click="
+      $colorMode.value = $colorMode.value === 'dark' ? 'light' : 'dark'
+      " />
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
