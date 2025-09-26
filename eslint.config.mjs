1ee599b9b407ac6027e23fc207fa929e65236a58
fabiangzvo <faguzman.97@gmail.com>
2025-09-26 11:02:49 -0500

    add correct eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import withNuxt from "./.nuxt/eslint.config.mjs";
 import tsParser from "@typescript-eslint/parser";
+import vueParser from "vue-eslint-parser";
 
 export default withNuxt({
   languageOptions: {
-    parser: tsParser,
+    parser: vueParser,
+    parserOptions: {
+      parser: tsParser,
+    },
   },
   rules: {
     "vue/multi-word-component-names": [
